d73ef8712cd898e300e3502aa126c0da21266b64
abrilelaboye <ilelaboyeabraham@gmail.com>
2022-12-09 06:31:28 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- *  * main - returns alphabets both except e and q
- *   *
- *    * Return: Always 0 (Success)
+  * main - returns alphabets both except e and q
+  *	
+  * Return: Always 0 (Success)
 */
 
 int main(void)
