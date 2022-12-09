4d2cfc895d3feadc594ad60d8899696bbb106091
abrilelaboye <ilelaboyeabraham@gmail.com>
2022-12-09 08:49:37 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,6 @@
 
 /**
   * main -  prints all possible different combinations of three digits.
-  *
   * Return: Always 0 (Success)
 */
 
@@ -24,7 +23,7 @@ int main(void)
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' 
+					if (!(ones == '9' && hundreds == '7' && 
 								tens == '8')) /*adds commas and spaces*/
 					{
 						putchar(',');
