ccf6422028c4a1d4322cef408e478bd2509a6b9f
abrilelaboye <ilelaboyeabraham@gmail.com>
2022-12-21 09:16:40 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,10 +19,10 @@ char *cap_string(char *s)
 			s[0] = s[0] - 32;
 		}
 		if (s[count] == ' ' || s[count] == '\t' || s[count] '\n'
-				|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-				|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-				|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-				|| s[count] == '{' || s[count] == '}')
+			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+			|| s[count] == '{' || s[count] == '}')
 		{
 			if (s[count + 1] >= 97 && s[count + 1] <= 122)
 			{
