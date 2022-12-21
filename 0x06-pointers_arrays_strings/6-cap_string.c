ddf43f6a9233753cabc5612f06c9839d27c31266
abrilelaboye <ilelaboyeabraham@gmail.com>
2022-12-21 10:26:54 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,30 @@
  *     * @s: POinter to string
  *      *
  *       * Return: Pointer to s
-  */
+*/
 char *cap_string(char *s)
 {
 	int count;
 
 	count = 0;
-	while (s[count] != '\0')
+while (s[count] != '\0')
+{
+	if (s[0] >= 97 && s[0] <= 122)
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-		{
-			s[0] = s[0] - 32;
-		}
-		if s[count] == ' ' || s[count] == '\t' || s[count] '\n'
-			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
-			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
-			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
-			|| s[count] == '{' || s[count] == '}'
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
-			{
-				s[count + 1] = s[count + 1] - 32;
-			}
-		}
-		count++;
+		s[0] = s[0] - 32;
 	}
-	return (s);
+	if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+	    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+	    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+	    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+	    || s[count] == '{' || s[count] == '}')
+	{
+		if (s[count + 1] >= 97 && s[count + 1] <= 122)
+		{
+			s[count + 1] = s[count + 1] - 32;
+		}
+	}
+	count++;
+}
+return (s);
 }
